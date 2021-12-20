7226e9e7ce8babb757021a352397c2f4c9bf715f
Enes Karakaş <enes.karakas.e@gamil.com>
2021-12-20 17:38:41 +0300

    setup server, rootQuery, userType

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,4 @@
-const qraphql = require('graphql');
+const graphql = require('graphql');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -8,19 +8,33 @@ const {
   GraphQLNonNull,
 } = graphql;
 
+const users = [
+  { id: 22, name: 'stephen', age: 25 },
+  { id: 44, name: 'Enes', age: 30 },
+];
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-    company: {
-      type: CompanyType,
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios
-          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
-          .then((res) => res.data);
+        return _.find(users, { id: args.id });
       },
     },
-  }),
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
+
 const app = express();
 
 app.use(
   '/graphql',
   expressGraphQL({
-    grapiql: true,
+    schema,
+    graphiql: true,
   })
 );
 
 app.listen(4000, () => {
-  console.log('listening on port 4000');
+  console.log('Listening');
 });
